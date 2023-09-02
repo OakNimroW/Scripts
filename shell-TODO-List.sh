97fca1e8702abb384ad9c5bb64894da813970144
OakNimroW <74124711+OakNimroW@users.noreply.github.com>
2023-09-02 19:17:20 -0300

    Update shell-TODO-List.sh
    change colors

--- shell-TODO-List.sh
+++ shell-TODO-List.sh
@@ -60,22 +60,22 @@ do
   elif [ $number -eq 2 ]
   then
 
-    echo -n -e "${purpleColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
+    echo -n -e "${yellowColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
 
   elif [ $number -eq 3 ]
   then
 
-    echo -n -e "${blueColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
+    echo -n -e "${turquoiseColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
 
   elif [ $number -eq 4 ]
   then
 
-    echo -n -e "${greenColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
+    echo -n -e "${purpleColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
 
   elif [ $number -eq 5 ]
   then
 
-    echo -n -e "${yellowColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
+    echo -n -e "${blueColor}[$number]${endColor} " && /usr/bin/bat --language=txt --style=plain --paging=never <(echo "$line" | /usr/bin/awk '{$1=""; $1=$1; sub(/^ */, ""); print}')
 
   else
 
